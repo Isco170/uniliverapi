82bb7211f35ec147c00914a4a2192d94418a5e48
Isco170 <franciscodamana@gmail.com>
2023-11-20 15:34:11 +0200

    Apagar data de 5 dias atras

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('uniliver_db', 'root', '', {
-//     repositoryMode: true,
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
-
-const sequelize = new Sequelize('UnileverAPI', 'admin', 'Tr0p1g4l1a', {
+const sequelize = new Sequelize('uniliver_db', 'root', '', {
     repositoryMode: true,
-    host: 'unilever-api.cdiz94kaxgxm.af-south-1.rds.amazonaws.com',
-    // port: 3306,
+    host: 'localhost',
     dialect: 'mysql'
 });
+
+// const sequelize = new Sequelize('UnileverAPI', 'admin', 'Tr0p1g4l1a', {
+//     repositoryMode: true,
+//     host: 'unilever-api.cdiz94kaxgxm.af-south-1.rds.amazonaws.com',
+//     // port: 3306,
+//     dialect: 'mysql'
+// });
 
 
 
